c4fd13250a33af583f5b2b1b0c8f252a9e4489b9
samarth <s4marthsharma@gmail.com>
2020-08-27 06:49:53 +0000

    update dburl

--- app.js
+++ app.js
@@ -20,9 +20,9 @@ var indexRoutes = require("./routes/index");
 
 
 
-//mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DATABASEURL, {useNewUrlParser: true, useUnifiedTopology: true});
 
-mongoose.connect("mongodb+srv://s4marth:s@marth1303@cluster0.boand.mongodb.net/Cluster0?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb+srv://s4marth:s@marth1303@cluster0.boand.mongodb.net/Cluster0?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
